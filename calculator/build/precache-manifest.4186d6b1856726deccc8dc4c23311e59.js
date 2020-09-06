6d7337507964ab26cd7d45b9b9461ef953766e31
Disha Patel <dishap1509@gmail.com>
2020-09-07 00:32:57 +0530

    Creating basic files to create app

---
+++ calculator/build/precache-manifest.4186d6b1856726deccc8dc4c23311e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5dc3d09d8d3e61f6105e533575231f1",
+    "url": "/dishap1515/react-app.git/index.html"
+  },
+  {
+    "revision": "e516a5bde92185491b64",
+    "url": "/dishap1515/react-app.git/static/css/main.2ac83395.chunk.css"
+  },
+  {
+    "revision": "e55ed680437e28c06a1d",
+    "url": "/dishap1515/react-app.git/static/js/2.66198967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dishap1515/react-app.git/static/js/2.66198967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e516a5bde92185491b64",
+    "url": "/dishap1515/react-app.git/static/js/main.5d6fea66.chunk.js"
+  },
+  {
+    "revision": "2ab9d018c96321759d64",
+    "url": "/dishap1515/react-app.git/static/js/runtime-main.426e77b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dishap1515/react-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
