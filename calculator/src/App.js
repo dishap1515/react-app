cbe7692126e4d6ae79714c4c78b375e1b0ff2595
Disha Patel <dishap1509@gmail.com>
2020-09-07 14:33:33 +0530

    Remove unwanted file

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-      
+        <div className="calc-wrapper">
+          <div className="row">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>/</button>
+          </div>
+          <div className="row">
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>*</button>
+          </div>
+          <div className="row">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+          </div>
+          <div className="row">
+            <button>.</button>
+            <button>0</button>
+            <button>=</button>
+            <button>-</button>
+          </div>
+        </div>
       </div>
     );
   }
